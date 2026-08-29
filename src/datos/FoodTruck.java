403610ac68a6568860cc725a044325e19947cfd4
BautistaAn <144617630+BautistaANun@users.noreply.github.com>
2026-08-29 17:59:06 -0300

    Mapeo de UnidadDeVenta (herencia joined-subclass) (#1)
    * Mapeo de UnidadDeVenta con herencia joined-subclass (FoodTruck y PuestoDesarmable)
    
    * Separa el acceso a datos en dao/negocio y agrega la validacion del codigo unico
    
    ---------
    
    Co-authored-by: BautistaANun <bautistaanunez0510@gmail.com>

---
+++ src/datos/FoodTruck.java
@@ -0,0 +1,39 @@
+package datos;
+
+public class FoodTruck extends UnidadDeVenta {
+
+	private String patente;
+	private boolean requiereElectricidad;
+
+	public FoodTruck() {
+	}
+
+	public FoodTruck(String nombre, double superficie, String codigo, String patente,
+			boolean requiereElectricidad) {
+		super(nombre, superficie, codigo);
+		this.patente = patente;
+		this.requiereElectricidad = requiereElectricidad;
+	}
+
+	public String getPatente() {
+		return patente;
+	}
+
+	public void setPatente(String patente) {
+		this.patente = patente;
+	}
+
+	public boolean isRequiereElectricidad() {
+		return requiereElectricidad;
+	}
+
+	public void setRequiereElectricidad(boolean requiereElectricidad) {
+		this.requiereElectricidad = requiereElectricidad;
+	}
+
+	@Override
+	public String toString() {
+		return "FoodTruck [id=" + id + ", nombre=" + nombre + ", superficie=" + superficie + ", codigo=" + codigo
+				+ ", patente=" + patente + ", requiereElectricidad=" + requiereElectricidad + "]";
+	}
+}
