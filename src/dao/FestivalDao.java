d7f2f4b0c5b14c0a75d42e84c2e9bbdd9dff0c0b
santiagomansilla <133055642+santiagomansilla@users.noreply.github.com>
2026-09-02 18:39:18 -0300

    test caso de uso festival (#16)
    * test caso de uso festival
    
    este test agrega un metodo en festivalDao y festivalABM para traer las unidades de venta de un festival, se agrega un nuevo test y se elimina otro llamado TestAgregarFestival, el cual era en donde trabajaba las cosas
    
    * update
    
    * Update README.md

--- src/dao/FestivalDao.java
+++ src/dao/FestivalDao.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 
 
 import java.util.List;
+
+import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -91,5 +93,26 @@ public class FestivalDao {
 		}
 		return lista;
 	}
+	
+	public Festival traerFestivalYUnidadDeVenta(int idFestival) throws HibernateException {
 
+	    Festival objeto = null;
+
+	    try {
+	        iniciaOperacion();
+
+	        String hql = "select distinct f from Festival f "
+	                   + "inner join fetch f.lstUnidad u "
+	                   + "where f.idFestival = :idFestival";
+
+	        objeto = (Festival) session.createQuery(hql).setParameter("idFestival", idFestival).uniqueResult();
+	        Hibernate.initialize(objeto.getLstUnidad());
+	    } finally {
+	        session.close();
+	    }
+
+	    return objeto;
+	}
+	          
+     
 }
