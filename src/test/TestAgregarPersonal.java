5d2933940b28dbe3d986a4e4737446d0f567b02b
Paluso <npalucito@hotmail.com>
2026-09-02 00:42:18 -0300

    Update TestAgregarPersonal.java

--- src/test/TestAgregarPersonal.java
+++ src/test/TestAgregarPersonal.java
@@ -18,6 +18,24 @@ public class TestAgregarPersonal {
 		} catch (Exception e) {
 			System.out.println("No se pude agragar Cajero: "+e.getMessage());
 		}
+		try {
+			idCajero=abm.agregarCajero("Marcela", "Roa", "44444444", LocalDate.of(1991, 5, 5), LocalDate.of(2020, 2, 15),"mañana");
+			System.out.printf("Alta Cajero id: %d%n",idCajero);
+		} catch (Exception e) {
+			System.out.println("No se pude agragar Cajero: "+e.getMessage());
+		}
+		try {
+			idCajero=abm.agregarCajero("Cecilia", "Sandoval", "55555555", LocalDate.of(2000, 7, 2), LocalDate.of(2019, 3, 1),"noche");
+			System.out.printf("Alta Cajero id: %d%n",idCajero);
+		} catch (Exception e) {
+			System.out.println("No se pude agragar Cajero: "+e.getMessage());
+		}
+		try {
+			idCajero=abm.agregarCajero("Sergio", "Perez", "66666666", LocalDate.of(2008, 11, 16), LocalDate.of(2024, 5, 15),"mañana");
+			System.out.printf("Alta Cajero id: %d%n",idCajero);
+		} catch (Exception e) {
+			System.out.println("No se pude agragar Cajero: "+e.getMessage());
+		}
 		
 		int idCocinero=-1;
 		try {
@@ -26,6 +44,21 @@ public class TestAgregarPersonal {
 		} catch (Exception e) {
 			System.out.println("No se puede cargar Cocinero: "+e.getMessage());
 		}
+
+		try {
+			idCocinero=abm.agregarCocinero("Juan", "Lopez", "11111111",LocalDate.of(2000, 9, 2),LocalDate.of(2022, 7, 1), "Maestro pizzero" ,20000 );
+			System.out.printf("Alta Cocinero id: %d%n",idCocinero);
+		} catch (Exception e) {
+			System.out.println("No se puede cargar Cocinero: "+e.getMessage());
+		}
+		try {
+			idCocinero=abm.agregarCocinero("Jose", "Gutierrez", "77777777",LocalDate.of(1980, 4, 12),LocalDate.of(2025, 5, 19), "Chef" ,40000 );
+			System.out.printf("Alta Cocinero id: %d%n",idCocinero);
+		} catch (Exception e) {
+			System.out.println("No se puede cargar Cocinero: "+e.getMessage());
+		}
+				
+
 	}
 
 }
